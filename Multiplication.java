5b8a10ce27dc31b558e581d197b050e904c72399
Suneel-B <suneelb@aec.edu.in>
2022-01-05 12:10:52 +0530

    import java.util.Scanner; class Demo {     public static void main(String[] args) {         Scanner scn = new Scanner(System.in);         int num1 = scn.nextInt();         int num2 = scn.nextInt();         scn.close();         int product = num1*num2;         System.out.println(product);     } }

--- Multiplication.java
+++ Multiplication.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 class Demo {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-        scan.close();
+        Scanner scn = new Scanner(System.in);
+        int num1 = scn.nextInt();
+        int num2 = scn.nextInt();
+        scn.close();
         int product = num1*num2;
         System.out.println(product);
     }
